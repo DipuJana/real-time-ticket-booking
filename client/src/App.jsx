b9ada5ea6bfc371323a4565f6b93b2b7877a5f40
Dipanjan Jana <dipanjanjana001@gmail.com>
2026-08-31 18:15:52 +0530

    chore: migrate client from TypeScript to JavaScript

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,16 +11,24 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img
+            src={heroImg}
+            className="base"
+            width="170"
+            height="179"
+            alt=""
+          />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
+
         <div>
           <h1>Get started</h1>
           <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
         </div>
+
         <button
           type="button"
           className="counter"
@@ -37,8 +45,10 @@ function App() {
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
+
           <h2>Documentation</h2>
           <p>Your questions, answered</p>
+
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
@@ -46,6 +56,7 @@ function App() {
                 Explore Vite
               </a>
             </li>
+
             <li>
               <a href="https://react.dev/" target="_blank">
                 <img className="button-icon" src={reactLogo} alt="" />
@@ -54,12 +65,15 @@ function App() {
             </li>
           </ul>
         </div>
+
         <div id="social">
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#social-icon"></use>
           </svg>
+
           <h2>Connect with us</h2>
           <p>Join the Vite community</p>
+
           <ul>
             <li>
               <a href="https://github.com/vitejs/vite" target="_blank">
@@ -73,6 +87,7 @@ function App() {
                 GitHub
               </a>
             </li>
+
             <li>
               <a href="https://chat.vite.dev/" target="_blank">
                 <svg
@@ -85,6 +100,7 @@ function App() {
                 Discord
               </a>
             </li>
+
             <li>
               <a href="https://x.com/vite_js" target="_blank">
                 <svg
@@ -97,6 +113,7 @@ function App() {
                 X.com
               </a>
             </li>
+
             <li>
               <a href="https://bsky.app/profile/vite.dev" target="_blank">
                 <svg
