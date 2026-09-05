a6d478db438a252b209b47c8ac56930111a2a4db
Indrajit Maity <indrajitmaity2005@gmail.com>
2026-09-05 23:29:40 +0530

    fix server..

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,14 @@
 import express from "express";
-import {venueRoutes} from './routes/venueRoutes.js';
 
-import inventoryRoutes from "./modules/inventory/inventory.routes.js";
+import {venueRoutes} from './routes/venueRoutes.js';
+import {eventRoutes} from './routes/eventRoutes.js';
+
 const app = express();
 
 // Use venue routes
 app.use('/api', venueRoutes);
+//Use event routes
+app.use('/api', eventRoutes);
 
 app.use(express.json());
 
--- server/src/config/env.js
+++ server/src/config/env.js
@@ -1,2 +1,6 @@
 import dotenv from 'dotenv';
+<<<<<<< HEAD
 dotenv.config({ path: "../.env" });
+=======
+dotenv.config({ path: "../.env" });
+>>>>>>> 4768746 (fix server..)
