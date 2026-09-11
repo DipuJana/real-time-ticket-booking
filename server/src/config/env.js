dac6d0aae13f251f5b81b1e90350606b8e299941
Indrajit Maity <indrajitmaity2005@gmail.com>
2026-09-11 21:37:26 +0530

    feat(event): implement event

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -1,9 +1,2 @@
 import dotenv from 'dotenv';
-<<<<<<< HEAD
 dotenv.config({ path: "../.env" });
-<<<<<<< HEAD
-=======
-dotenv.config({ path: "../.env" });
->>>>>>> 4768746 (fix server..)
-=======
->>>>>>> feature/event
