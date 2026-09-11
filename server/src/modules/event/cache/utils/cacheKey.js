4ea6b4271a8c9bd562b5f047217426640d0994d8
Indrajit Maity <indrajitmaity2005@gmail.com>
2026-09-11 20:48:48 +0530

    fix:local branch

--- server/src/modules/event/cache/utils/cacheKey.js
+++ server/src/modules/event/cache/utils/cacheKey.js
@@ -15,33 +15,4 @@ export class CacheKeys {
     return venueId ? `${this.prefix}:${this.version}:venue:${venueId}` : `${this.prefix}:${this.version}:venue:*`;
   }
 
-  // Event cache
-
-  static event(id) {
-    return `${this.prefix}:${this.version}:event:${id}`;
-  }
-  static eventsList(filter) {
-    return `${this.prefix}:${this.version}:events:${filter}`;
-  }
-  static eventsSearch(query) {
-    return `${this.prefix}:${this.version}:events:search:${query}`;
-  }
-  static eventsByCategory(category) {
-    return `${this.prefix}:${this.version}:events:category:${category}`;
-  }
-  static eventsByCity(city) {
-    return `${this.prefix}:${this.version}:events:city:${city}`;
-  }
-
-  static upcomingEvents(date){
-    return `${this.prefix}:${this.version}:events:upcoming:${date}`;
-  }
-
-  static eventDetail(eventId) {
-    return `${this.prefix}:${this.version}:event:detail:${eventId}`;
-  }
-  static eventPattern(eventId) {
-    return eventId ? `${this.prefix}:${this.version}:event:*${eventId}*` : `${this.prefix}:${this.version}:event:*`;
-  }
-
 }
